ab89068045afc46d86b2ca77e0fc69bc7475e48b
Dev Akash <devakashlakshmanan@gmail.com>
2026-09-16 20:59:51 +0530

    Initial commit

---
+++ src/main/java/AIDSA/project/StudentRepository.java
@@ -0,0 +1,7 @@
+package AIDSA.project;
+import AIDSA.project.Models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
---
+++ src/test/java/AIDSA/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package AIDSA.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
